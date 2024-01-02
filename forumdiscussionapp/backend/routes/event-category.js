84e9a1452724e4ef077bb110c1e128655f803412
Tejas Suresh <tejassuresh98@gmail.com>
2024-01-02 12:16:05 -0500

    events-category complete

--- forumdiscussionapp/backend/routes/event-category.js
+++ forumdiscussionapp/backend/routes/event-category.js
@@ -2,16 +2,19 @@ const express = require('express');
 const router = express.Router();
 const { verifyJwt } = require('../authvalid');
 const {
-    validateAssignEventCategory,
-  } = require('../body-validation/event-validation');
+  validateCreateEventCategory,
+  validateEditEventCategory,
+  validateAssignEventCategory,
+  validateEditAssignedEventCategory,
+} = require('../body-validation/event-category-validation');
 
-const {createEventCategory} =require('../event-routes/create-event-category')
-const {getEventCategories} =require('../event-routes/get-event-categories')
-const {editEventCategory} =require('../event-routes/edit-event-category')
-const {softDeleteEventCategory} =require('../event-routes/delete-event-category')
-const {assignEventCategory} =require('../event-routes/assign-event-category')
-const {editAssignedEventCategory} =require('../event-routes/edit-assigned-event-category')
-const {softDeleteAssignedEventCategory} =require('../event-routes/delete-assigned-event-category')
+const {createEventCategory} =require('../event-category-routes/create-event-category')
+const {getEventCategories} =require('../event-category-routes/get-event-categories')
+const {editEventCategory} =require('../event-category-routes/edit-event-category')
+const {softDeleteEventCategory} =require('../event-category-routes/delete-event-category')
+const {assignEventCategory} =require('../event-category-routes/assign-event-category')
+const {editAssignedEventCategory} =require('../event-category-routes/edit-assigned-event-category')
+const {softDeleteAssignedEventCategory} =require('../event-category-routes/delete-assigned-event-category')
 
 router.use(express.json());
 
@@ -19,10 +22,10 @@ router.use(express.json());
 router.get('/event-categories', verifyJwt, getEventCategories);
 
 // Endpoint to create a new event category
-router.post('/event-categories/create', verifyJwt, createEventCategory);
+router.post('/event-categories/create', verifyJwt, validateCreateEventCategory, createEventCategory);
 
 // Endpoint to edit an existing event category
-router.put('/event-categories/edit/:categoryId', verifyJwt, editEventCategory);
+router.put('/event-categories/edit/:categoryId', verifyJwt, validateEditEventCategory, editEventCategory);
 
 // Endpoint to soft delete an event category
 router.delete('/event-categories/soft-delete/:categoryId', verifyJwt, softDeleteEventCategory);
@@ -31,7 +34,7 @@ router.delete('/event-categories/soft-delete/:categoryId', verifyJwt, softDelete
 router.post('/events/:eventId/categories/assign/:categoryId', verifyJwt, validateAssignEventCategory, assignEventCategory);
 
 // Endpoint to edit an assigned event category
-router.put('/events/:eventId/categories/edit/:categoryId', verifyJwt, editAssignedEventCategory);
+router.put('/events/:eventId/categories/edit/:categoryId', verifyJwt, validateEditAssignedEventCategory, editAssignedEventCategory);
 
 // Endpoint to soft delete an assigned event category
 router.delete('/events/:eventId/categories/soft-delete/:categoryId', verifyJwt, softDeleteAssignedEventCategory);
